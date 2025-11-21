d2e1957d69638e3b1f3521a0f42e1f48ae3f93c3
Benedicte de Feuardent <keryxiaa@gmail.com>
2025-11-21 10:36:41 +0100

    Fix account routes db/sqlite paths

--- routes/account/account.me.js
+++ routes/account/account.me.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // Chemins corrects depuis routes/account/
-const db = require('../../lib/db');
+const db = require('../../db/sqlite');
 const { requireAuth } = require('../../middleware/auth');
 
 // ======================================
